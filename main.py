93ebbd0f9d9a693d7608aca34d0a9861621d149c
mohammad_s <mohammad_s@gmail.com>
2023-05-07 15:10:24 +0330

    scaduling and error handeling

--- main.py
+++ main.py
@@ -3,23 +3,20 @@ import finpy_tse as fpy
 
 if __name__=="__main__":
 
-    init_price_args = {'ignore_date':True,'adjust_price':False,'show_weekday':False,'double_date':True}
-    update_price_args = {'ignore_date':False,'adjust_price':False,'show_weekday':False,'double_date':True}
+    init_price_args = {'ignore_date':True,'adjust_price':False,'show_weekday':True,'double_date':True}
+    update_price_args = {'ignore_date':False,'adjust_price':False,'show_weekday':True,'double_date':True}
     Get_Price_History = fpy.Get_Price_History
 
-    init_IR_args = {'ignore_date':True, 'show_weekday':False, 'double_date':True}
-    update_IR_args = {'ignore_date':False, 'show_weekday':False, 'double_date':True}
+    init_IR_args = {'ignore_date':True, 'show_weekday':True, 'double_date':True}
+    update_IR_args = {'ignore_date':False, 'show_weekday':True, 'double_date':True}
     Get_IR_History = fpy.Get_RI_History
 
-    p_e = DataExtractor(collection_name='price', Extractor_Func=Get_Price_History, init_args=init_price_args, update_args=update_price_args)
-    ir_e = DataExtractor(collection_name='IR', Extractor_Func=Get_IR_History, init_args=init_IR_args, update_args=update_IR_args)
+    price_extractor = DataExtractor(collection_name='price', Extractor_Func=Get_Price_History, init_args=init_price_args, update_args=update_price_args)
+    ir_extractor = DataExtractor(collection_name='IR', Extractor_Func=Get_IR_History, init_args=init_IR_args, update_args=update_IR_args)
 
-    p_e.start()
-    ir_e.start()
+    price_extractor.start()
+    ir_extractor.start()
 
-    p_e.join()
-    ir_e.join()
-
-    print("Price:", p_e.stock_error_list)
-    print("IR:", ir_e.stock_error_list)
+    price_extractor.join()
+    ir_extractor.join()
 
--- tsetmcExtractor.py
+++ tsetmcExtractor.py
@@ -6,7 +6,7 @@ from stocks.stocksHandler import stockIndexing
 import jdatetime
 import signal
 import threading
-
+import time
 def signal_handler(signum, frame):
     raise Exception("Timed out!")
 signal.signal(signal.SIGALRM, signal_handler)
@@ -27,47 +27,56 @@ class DataExtractor(threading.Thread):
 
         self.query_utils = QueryUtils(self.collection)
 
-        self.stock_error_list = []
-
     def run(self):
+        sleep_time = 2
+        stocks_not_found = self.stk_idx_lst
+        while (len(stocks_not_found) != 0 and sleep_time < 100):
+            stoks_errors = []
+            for stock_idx in stocks_not_found:
+                stock_name = self.stock_indexing.idx2stock(stock_idx)
+                try:
+                    signal.alarm(30)
+                    stk_lst_data = self.query_utils.StockLastData(stock_name)
+                    if len(stk_lst_data) == 0:
+                        self.__initInsert(stock_name)
+                    else:
+                        self.__insert(stock_name, stk_lst_data)
+                except:
+                    print(stock_name, " error")
+                    stoks_errors.append(stock_idx)
+            print("errors num: ",len(stoks_errors))
+            stocks_not_found = stoks_errors.copy()
+            time.sleep(sleep_time)
+            sleep_time += 2
 
-        for stock in self.stk_idx_lst:
-            stk_name = self.stock_indexing.idx2stock(stock)
-            try:
-                signal.alarm(30)
-                stk_lst_data = self.query_utils.StockLastData(stk_name)
-                if len(stk_lst_data) == 0:
-                    self.__initInsert(stk_name)
-                else:
-                    self.__insert(stk_name, stk_lst_data)
-            except:
-                print(stk_name, " error")
-                self.stock_error_list.append(stk_name)
         return
 
     def __initInsert(self, stk_name):
         init_args = self.init_args.copy()
         init_args.update({"stock": stk_name})
         df = self.Extractor_Func(**init_args)
-        df['Jdate'] = df.index
+        df['Jal_Date'] = df.index
+        df['Index'] = [self.stock_indexing.stock2idx(stk_name)] * len(df)
         self.query_utils.inserertDF(df)
-        print("first inserted: ", stk_name, len(df))
+        print("first inserted: ", stk_name)
 
     def __insert(self, stk_name, stk_lst_data):
-        stk_lst_date = stk_lst_data[0]['Jdate']
+        stk_lst_date = stk_lst_data[0]['Jal_Date']
         current_date = jdatetime.date.today()
         if stk_lst_date != str(current_date):
             start_date = str((jdatetime.datetime.strptime(stk_lst_date, "%Y-%m-%d") + jdatetime.timedelta(days=1)).date())
-            end_date = str(current_date - jdatetime.timedelta(days=1))
+            end_date = str(current_date)
+            # end_date = str(current_date - jdatetime.timedelta(days=1))
             update_args = self.update_args.copy()
             update_args.update({"stock": stk_name, "start_date": start_date, "end_date": end_date})
             df = self.Extractor_Func(**update_args)
             if len(df) > 0:
-                df['Jdate'] = df.index
+                df['Jal_Date'] = df.index
+                df['Index'] = [self.stock_indexing.stock2idx(stk_name)] * len(df)
                 self.query_utils.inserertDF(df)
-                print("update: ", stk_name, len(df))
+                print("update: ", stk_name)
             else:
-                print("today not found: ", stk_name, len(df))
+                print("no new value : ", stk_name)
         else:
             print(stk_name, " is existing")
 
